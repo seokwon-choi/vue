7692c2f899858739bdcc9795e146c12009e81942
seokwon-choi <bluecircle2290@gmail.com>
2022-03-30 11:25:36 +0900

    router

--- part2/ch3/src/App.vue
+++ part2/ch3/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <router-view></router-view>
+    <SimpleComp></SimpleComp>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
+import SimpleComp from './components/SimpleComp.vue'
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SimpleComp
   }
 }
 </script>
