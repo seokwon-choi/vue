52d2e6e86246f009c0820ca1d22ba94000a20fc5
seokwon-choi <bluecircle2290@gmail.com>
2022-03-30 10:40:23 +0900

    ch3

--- part2/ch3/src/App.vue
+++ part2/ch3/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    | <router-link to = "/">Go to Home</router-link> | <br>
-    | <router-link to = "/login">Go to Login</router-link> | 
-    <router-view></router-view>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    HelloWorld
   }
 }
 </script>
