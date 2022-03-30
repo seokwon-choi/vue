df338846f80254e17a7c493209ff242161f52b73
seokwon-choi <bluecircle2290@gmail.com>
2022-03-30 16:34:42 +0900

    web

---
+++ part2/web/routes/api.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/user/:id', function(req, res){
+  const id = req.params.id;
+  const obj = {
+    id: id,
+    name: 'John',
+    age: 30,
+    job: 'developer',
+    city: 'New York'
+  };
+  res.json(obj);
+});
+
+module.exports = router;
