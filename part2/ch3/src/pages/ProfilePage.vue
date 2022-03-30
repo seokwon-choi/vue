e4d65d09ab27a216cd0776e17eebee3c1950dfea
seokwon-choi <bluecircle2290@gmail.com>
2022-03-30 15:19:24 +0900

    login

--- part2/ch3/src/pages/ProfilePage.vue
+++ part2/ch3/src/pages/ProfilePage.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
-    <h1>프로파일 입니다.</h1>
+  <div class="profile">
+    <h1>Profile</h1>
+    <div>{{ email }}</div>
+    <button @click="logout">log out</button>
   </div>
 </template>
 
 <script>
-export default{
+export default {
+  data() {
+    return {
+      email: localStorage.getItem('email')
+    }
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token')
+      localStorage.removeItem('email')
+      this.$router.push('/')
+    }
+  }
 }
 </script>
